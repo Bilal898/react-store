1c4d071c0deca923b0ad0530bd3475104217f54b
bilal898 <bfhamid@gmail.com>
2019-10-29 17:22:54 +0500

    cart, index links added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Cart from './components/Cart'
+import Details from './components/Details'
+import Default from './components/Default'
 function App() {
   return (
       <React.Fragment>
-        <h3>hello from app</h3>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route exact path="/details" component={Details} />
+          <Route exact path="/cart" component={Cart} />
+          <Route  component={Default} />
+        </Switch>
+        
       </React.Fragment>
   );
 }

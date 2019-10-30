f771ee6806e9c7c5ea31a10f5ceec40d83e62b1f
bilal898 <bfhamid@gmail.com>
2019-10-30 19:03:53 +0500

    each product detail shown on click 3.24

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,20 +3,25 @@ import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 import {ProductConsumer} from '../Context'
 import { log } from 'util';
+import propTypes from 'prop-types'
 export default class Product extends Component {
     render() {
         const {id, title, img, price, inCart} = this.props.product
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={() => console.log('you clicked me on image')
+                  <ProductConsumer>
+                    {(value) => (
+                      <div className="img-container p-5" onClick={() => 
+                        value.handleDetail(id)
                     }>
                         <Link to="/details">
                             <img src={img} alt="product" className="card-img-top" />
                         </Link>
                         <button className="cart-btn" 
                         disabled={inCart?true:false} 
-                        onClick={() => {console.log("added to cart");
+                        onClick={() => {
+                          value.addToCart(id)
                         }}
                         >
                         {inCart ? 
@@ -29,6 +34,10 @@ export default class Product extends Component {
                         
 
                     </div>
+                    )}
+                    
+                    </ProductConsumer>
+
                     {/* card footer */}
                     <div className="card-footer d-flex justify-content-between">
                         <p className="align-self-center mb-0">{title}</p>
@@ -41,6 +50,16 @@ export default class Product extends Component {
             </ProductWrapper>
         )
     }
+}
+
+Product.propTypes = {
+    product:propTypes.shape({
+        id:propTypes.number,
+        img:propTypes.string,
+        title:propTypes.string,
+        price:propTypes.number,
+        inCart:propTypes.bool
+    }).isRequired
 }
 
 const ProductWrapper = styled.div`
